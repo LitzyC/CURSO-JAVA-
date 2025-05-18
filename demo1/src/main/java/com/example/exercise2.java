028b9bf40903b3288b8c4436201c5d915ce99b7a
Litzy Cabrera <87455748+LitzyC@users.noreply.github.com>
2025-05-18 09:00:39 +0200

    Exercise 2
    Explicado cada línea

--- demo1/src/main/java/com/example/exercise2.java
+++ demo1/src/main/java/com/example/exercise2.java
@@ -2,16 +2,27 @@ package com.example;
 
 import java.util.Scanner;
 
-public class exercise2 {
+public class Exercise2 {
     
-    private static void Palindromo() {
-        Scanner letra = new Scanner(System.in);
-        letra.next();
-        //System.out.println(palabra);
+    private static void palindromo() {
+        Scanner lectura = new Scanner(System.in);
+        //Lee los datos que ingresamos
+        System.out.println("Ingresa la palabra.");
+        String palabra = lectura.nextLine(); 
+        //Se guarda el dato que introducimos en palabra
+        String palabrainvertida = new StringBuilder(palabra).reverse().toString();
+        //new StringBuilder() --- Nos permite crear un objeto y poderlo editar
+        //.toString() --- lo vueelve a texto normal
         
-
+        if (palabra.equals(palabrainvertida)){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+        //Equals permite comparar
     }
+
     public static void main(String[] args) {
-        Palindromo();
+        palindromo();
     }
 }

efb458c29f45993efb6aa769551e1d159d39762c
Litzy Cabrera <87455748+LitzyC@users.noreply.github.com>
2025-05-19 11:21:12 +0200

    Exercise 1 - Factorial
    Realizar factorial de un numero.
    Dentro de comentarios:
    Mostrar por consola la cadena de como se hace el factorial.
    Se usa Integer.toString() para devolverlo en texto y así se muestre el como se hace.
    Detallado.

--- demo1/src/main/java/com/example/Exercise1.java
+++ demo1/src/main/java/com/example/Exercise1.java
@@ -4,17 +4,32 @@ import java.util.Scanner;
 
 public class Exercise1 {
     
-    public static void factorial() {
+    public static void factorial(){
         Scanner lector = new Scanner(System.in);
-        System.out.println("Put the number");
-        int number=lector.nextInt();
-        System.out.println(number);
+        System.out.println("Ingresa el número a factorizar: ");
+        int numero=lector.nextInt();
+        
+        int numeroFactorial=1;//Guardamos el valor del factorial
+
+        //String cadena = Integer.toString(numero) + "! = 1";
+        //Composicion de la cadena con el resultado
+        //Muestra en tipo texto --- numero ! = 1
+
+        for (int num = 2; num <= numero; num++) {
+            numeroFactorial=numeroFactorial* num;
+            //Multiplica todos los numeros 
+            //cadena=cadena + " x " + Integer.toString(num);
+            //Integer --- es la clase de int, representa un numero entero
+            //toString() --- devuelve una cadena de texto
+            //Muestra por consola la cadena del numero hecho factorial en texto
+            //Muestra --- cadena x 2 x 3 x 4 x 5 
+        }
+        
+        System.out.println("El número factorial de: " + numero+" es: "+ numeroFactorial);
+        //System.out.println(cadena + " = " + Integer.toString(numeroFactorial));
         
     }
-
-
     public static void main(String[] args) {
         factorial();
     }
-    
 }

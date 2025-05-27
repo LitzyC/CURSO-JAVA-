01a538d1e404e75b0a0647f13edd65908baafa33
Litzy Cabrera <87455748+LitzyC@users.noreply.github.com>
2025-05-27 23:09:45 +0200

    Lista de Tareas - Ejercicio 3
    Ejercicio de POO

--- POO/listatareas/src/main/java/com/example/Main.java
+++ POO/listatareas/src/main/java/com/example/Main.java
@@ -1,7 +1,36 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Tarea> listaTareas = new ArrayList<>();
+
+        listaTareas.add(new Tarea("Hacer Matmáticas |","Ejercicios 1 - 10 |",true));
+        listaTareas.add(new Tarea("Hacer ejercicios |","Correr 5min |",false));
+        listaTareas.add(new Tarea("Cocinar |"," Preparar la Merienda |",true));
+        
+        System.out.println("----TODAS LAS TAREAS----");
+        for (Tarea tarea : listaTareas) {
+            tarea.mostrar();
+        }
+
+        int completadas=0;
+        for (Tarea tarea : listaTareas) {
+            if (tarea.marcarTarea()) {
+                completadas++;
+            }
+        }
+
+        System.out.println("TAREAS COMPLETADAS: " + completadas);
+        
+        System.out.println("TAREAS PENDIENTES: ");
+
+        for(Tarea tarea:listaTareas){
+            if(!tarea.marcarTarea()){
+                tarea.mostrar();
+            }
+        }
+    
     }
 }
--- POO/listatareas/src/main/java/com/example/Tarea.java
+++ POO/listatareas/src/main/java/com/example/Tarea.java
@@ -8,20 +8,67 @@ public class Tarea {
 
     String titulo;
     String descripcion;
-    boolean estado;
+    private Estado estado;
 
-    public Tarea(String titulo, String descripcion,boolean estado){
+    private static Tarea tarea1;
+    private static Tarea tarea2;
+    private static Tarea tarea3;
+
+    public Tarea(String titulo, String descripcion,boolean estaCompletada) {
         this.titulo=titulo;
         this.descripcion=descripcion;
-        this.estado=false;
+        this.estado=estaCompletada? Estado.COMPLETADA : Estado.PENDIENTE;
     }
 
-    public void marcarTarea(){
-        this.estado=true;
+    public boolean marcarTarea(){
+        return this.estado == Estado.COMPLETADA;
     }
 
     public void mostrar(){
-        String completo=estado?"Tarea Completada":"Pendiente";
-        System.out.println("Título: "+titulo+ " Descripción: "+descripcion+completo);
+        String tareaEstado = (estado == Estado.COMPLETADA) ? "Tarea Completada" : "Tarea Pendiente";
+        System.out.println("Título: " + titulo + " Descripción: " + descripcion + tareaEstado);
+    }
+
+
+    private static boolean assertEquals(boolean esperado, boolean actual){
+        return esperado == actual;
+    }
+
+    private static void testTareaCompleta(){
+        if(!assertEquals(true, tarea1.marcarTarea())){
+            System.out.println("Error: Test tarea completada.");
+        } else {
+            System.out.println("Funciona el test de tarea completada.");
+        }
+    }
+
+    private static void testTareaPendiente(){
+        if(!assertEquals(false, tarea2.marcarTarea())){
+            System.out.println("Error: Test tarea pendiente.");
+        } else {
+            System.out.println("Funciona el test de tarea pendiente.");
+        }
+    }
+
+    private static void testMarcarTarea(){
+        if(!assertEquals(true, tarea3.marcarTarea())){
+            System.out.println("Error: Test marcar la tarea.");
+        } else {
+            System.out.println("Funciona el test de marcar tarea.");
+        }
+    }
+
+    private static void testTarea() {
+        tarea1 = new Tarea("Hacer Matmáticas |", "Ejercicios 1 - 10 |", true);
+        tarea2 = new Tarea("Hacer ejercicios |", "Correr 5min |", false);
+        tarea3 = new Tarea("Cocinar |", "Preparar la Merienda |", true);
+
+        testMarcarTarea();
+        testTareaCompleta();
+        testTareaPendiente();
+    }
+
+    public static void main(String[] args) {
+        testTarea();
     }
 }

add4eb704185e925aae434951747a198a6c9ddfc
Litzy Cabrera <87455748+LitzyC@users.noreply.github.com>
2025-05-28 21:03:56 +0200

    Mascota Virtual.
    Ejercicio 5 de la clase de Herencias.
    
    Continuar revisando.

--- HERENCIAS/mascotasvirtuales/src/main/java/com/MascotaVirtual.java
+++ HERENCIAS/mascotasvirtuales/src/main/java/com/MascotaVirtual.java
@@ -16,19 +16,29 @@ public class MascotaVirtual {
 
     public void comer(){
         energia += 10;
-        felicidad +=10;
         if (energia>100){
             energia=100;
             System.out.println("Tu mascota esta por reventar de tanta comida.");
         }
+        felicidad +=10;
+        if (felicidad > 100) {felicidad = 100;}
     }
 
     public void jugar(){
         energia -= 10;
+        if(energia>100){energia = 100;}
+        
         felicidad += 10;
+        if (felicidad > 100) {felicidad = 100;}
+        
+        if (energia < 0) {
+            energia = 0;
+            System.out.println("Tu "+nombre +" está muy cansado para jugar.");
+        }
     }
 
     public void mostrarEstado(){
+        System.out.println("-------");
         System.out.println("Nombre: " + nombre);
         System.out.println("Energía: " + energia);
         System.out.println("Felicidad: " + felicidad);
@@ -36,6 +46,7 @@ public class MascotaVirtual {
 
     public void menuVirtual(){
         Scanner lectura = new Scanner (System.in);
+        MascotaVirtual mascotaSeleccionada = null;
         
         System.out.println("--- :3 MASCOTA VIRTUAL :3 ---");
         System.out.println(" ");
@@ -43,11 +54,9 @@ public class MascotaVirtual {
         System.out.println("\n---1.Perro 🐕---");
         System.out.println("---2.Gato 🐈 ---");
         System.out.println("---3.Hamster 🐹 ---");
-        
+        System.out.print("\nSeleccione una opción: ");
         int mascotaTipo=lectura.nextInt();
         lectura.nextLine();
-        MascotaVirtual mascotaSeleccionada = null;
-
         switch (mascotaTipo) {
             case 1:
                 mascotaSeleccionada= new PerroVirtual("Perro Virtual");
@@ -62,9 +71,11 @@ public class MascotaVirtual {
         
             default:
                 System.out.println("Tipo de mascota no reconocido.");
-                break;
+                lectura.close();
+                return;
         }
 
+        int opcion;
         do{
             System.out.println("\n--- Menú de Mascota Virtual ---");
             System.out.println("\n1. Jugar");
@@ -72,8 +83,10 @@ public class MascotaVirtual {
             System.out.println("3. Mostrar Estado");
             System.out.println("4. Salir");
             System.out.print("\nSeleccione una opción: ");
-            int opcion=lectura.nextInt();
-
+        
+            
+            opcion=lectura.nextInt();
+            
             switch (opcion) {
                     case 1:
                         mascotaSeleccionada.jugar();
@@ -90,9 +103,10 @@ public class MascotaVirtual {
                     default:
                         System.out.println("Opción no válida, intente de nuevo.");
                 }
-            lectura.close();
+            } while (opcion !=4);
+        
             System.out.println("\n--- Fin de la Aplicación. ---");
-        } while (true);
+            lectura.close();
     }    
 }
 
@@ -100,13 +114,22 @@ class PerroVirtual extends MascotaVirtual{
 
     public PerroVirtual(String nombre){
         super(nombre);
-        mostrarEstado();
     }
 
     @Override
     public void jugar(){
         energia -= 10;
+        if (energia>100) {energia = 100;}
+        if (energia < 0) {
+            energia = 0;
+            System.out.println("Tu perrito está muy cansado para jugar.");
+            return;
+        }
+        
         felicidad += 20;
+        if (felicidad > 100) {felicidad = 100;}
+        
+        System.out.println("Tu perrito está jugando");
     }
 
 }
@@ -115,17 +138,17 @@ class GatoVirtual extends MascotaVirtual{
 
     public GatoVirtual(String nombre){
         super(nombre);
-        mostrarEstado();
     }
 
     @Override
     public void comer(){
         energia += 20;
-        felicidad += 10;
         if (energia>100){
             energia=100;
-            System.out.println("Tu mascota esta por reventar de tanta comida.");
+            System.out.println("Tu gatito esta por reventar de tanta comida.");
         }
+        felicidad += 10;
+        if (felicidad > 100) {felicidad = 100;}
     }
 }
 
@@ -133,12 +156,17 @@ class HamsterVirtual extends MascotaVirtual{
 
     public HamsterVirtual(String nombre){
         super(nombre);
-        mostrarEstado();
     }
 
     @Override
     public void jugar(){
         energia -= 30;
+        if(energia < 0) {
+            energia = 0;
+            System.out.println("Tu hámster se agota muy rápido, se más cuidadoso.");
+        }
+        if(energia > 100) {energia = 100;}
         felicidad += 20;
+        if(felicidad > 100) {felicidad = 100;}
     }
 }

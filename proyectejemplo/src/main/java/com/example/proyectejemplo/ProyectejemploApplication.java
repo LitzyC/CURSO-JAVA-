84384b28865cc64a05e3683c6baaf6ffed12e4c7
Litzy Cabrera <87455748+LitzyC@users.noreply.github.com>
2025-06-30 12:44:02 +0200

    Proyecto Ejemplo

---
+++ proyectejemplo/src/main/java/com/example/proyectejemplo/ProyectejemploApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectejemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectejemploApplication.class, args);
+	}
+
+}
---
+++ proyectejemplo/src/test/java/com/example/proyectejemplo/ProyectejemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectejemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
